43344404355de7ce8351ea2b7742270bbf698825
Jeff <1015005300@qq.com>
2019-06-26 17:31:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route('/nlp/v1/health', methods=['GET'])
 def hello():
     return "I'm doing great!"
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=8001)
