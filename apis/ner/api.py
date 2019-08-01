8f8cfaa76cc607a62d9d68ae88295b39d44255e7
Jeff <1015005300@qq.com>
2019-08-01 18:59:37 +0800

    Update api.py

--- apis/ner/api.py
+++ apis/ner/api.py
@@ -30,7 +30,8 @@ api_bp = NLPBlueprint('ner', __name__, init, url_prefix='/nlp/v1/ner')
 def ner_dashboard():
     if request.method == 'GET':
         text = """
-        Asian shares skidded on Tuesday after a rout in tech stocks put Wall Street to the sword.
+        When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week.
+        A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.
         """
     elif request.method == 'POST':
         text = request.form.get('text_area')
