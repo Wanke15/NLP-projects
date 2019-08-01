5069b7c5fa05274b03e48737d68042467336e0b7
jeff <jeff.wang@ribitwo.com>
2019-08-01 18:41:31 +0800

    update ner and key_words UT

--- tests/ut/key_words/test_key_words.py
+++ tests/ut/key_words/test_key_words.py
@@ -4,27 +4,26 @@ from key_words.key_words_extraction import KeywordExtractor
 
 
 class TestKeyWords(unittest.TestCase):
+    def setUp(self):
+        KeywordExtractor.init(['tfidf', 'textrank'])
+
     def test_tfidf(self):
         test_text = 'RAKE算法用来做关键词(keyword)的提取，实际上提取的是关键的短语(phrase)'
-        KeywordExtractor.init('tfidf')
-        res = KeywordExtractor.extract(test_text)
+        res = KeywordExtractor.get_instance('tfidf').extract(test_text)
         print(res)
 
     def test_textrank(self):
         test_text = 'RAKE算法用来做关键词(keyword)的提取，实际上提取的是关键的短语(phrase)'
-        KeywordExtractor.init('textrank')
-        res = KeywordExtractor.extract(test_text)
+        res = KeywordExtractor.get_instance('textrank').extract(test_text)
         print(res)
 
     def test_use_pos(self):
         test_text = 'RAKE算法用来做关键词(keyword)的提取，实际上提取的是关键的短语(phrase)'
         pos = ['n', 'v']
-        KeywordExtractor.init('tfidf')
-        print(KeywordExtractor.extract(test_text))
-        res = KeywordExtractor.extract(test_text, pos=pos)
+        res = KeywordExtractor.get_instance('textrank').extract(test_text, pos=pos)
         print(res)
 
     def test_not_implemented(self):
         with self.assertRaises(NotImplementedError) as cm:
-            KeywordExtractor.init('lda')
+            KeywordExtractor.init(['tfidf', 'textrank', 'lda'])
         self.assertEqual(str(cm.exception), 'Extractor not supported!')
--- tests/ut/ner/test_ner.py
+++ tests/ut/ner/test_ner.py
@@ -1,12 +1,14 @@
+import os
 import unittest
 
 from ner.ner_parse import NER
 from ner.render import render_entities_content
+from utils import get_base_dir
 
 
 class TestNER(unittest.TestCase):
     def setUp(self):
-        NER.init('en_core_web_sm')
+        NER.init(os.path.join(get_base_dir(), 'resources/models/ner/spacy_model'))
 
     def test_ner_parse(self):
         text = 'Asian shares skidded on Tuesday after a rout in tech stocks put Wall Street to the sword'
